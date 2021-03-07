888b43777d0d9e382ccb43d1ec997f27dddb59d9
Sami <s.m.sami125@gmail.com>
2021-03-07 21:34:42 +0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,77 @@
-import '../styles/globals.css'
+import React, { useState, useCallback } from 'react';
+import '@shopify/polaris/dist/styles.css';
+import enTranslations from '@shopify/polaris/locales/en.json';
+import {
+  AppProvider,
+  Page,
+  Button,
+  Popover,
+  ActionList,
+  Stack,
+  Heading,
+  DisplayText,
+} from '@shopify/polaris';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [helpMenu, setHelpMenu] = useState(false);
+
+  //This will toggle help menu
+  const toggleHelpMenu = useCallback(
+    () => setHelpMenu((active) => !active),
+    []
+  );
+  const activator = (
+    <Button onClick={toggleHelpMenu} disclosure>
+      Help
+    </Button>
+  );
+
+  return (
+    <AppProvider i18n={enTranslations}>
+      <Page>
+        <DisplayText size="large">Textfield for Product Page</DisplayText>
+        <Stack distribution="fillEvenly">
+          <Stack.Item>
+            <Heading element="h2">
+              The simple solution for adding a textfield to the product.
+            </Heading>
+          </Stack.Item>
+          <Stack.Item>
+            <div style={{ float: 'right' }}>
+              <Popover
+                active={helpMenu}
+                activator={activator}
+                onClose={toggleHelpMenu}
+              >
+                <ActionList
+                  items={[
+                    {
+                      content: 'Import file',
+                      onAction: () => console.log('Imported action'),
+                    },
+                    {
+                      content: 'Export file',
+                      onAction: () => console.log('Exported action'),
+                    },
+                  ]}
+                />
+              </Popover>
+            </div>
+          </Stack.Item>
+        </Stack>
+        <div
+          style={{
+            borderBottom: '0.3px solid grey',
+            opacity: 0.3,
+            width: '100%',
+            padding: 0,
+            marginBottom: '2em',
+          }}
+        />
+        <Component {...pageProps} />
+      </Page>
+    </AppProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,129 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import {
+  Button,
+  Icon,
+  Stack,
+  Heading,
+  DisplayText,
+  Card,
+  Layout,
+  TextField,
+  FormLayout,
+} from '@shopify/polaris';
+
+import { HomeMajor } from '@shopify/polaris-icons';
 
 export default function Home() {
+  const [fieldName, setFieldName] = useState('');
+  const [fieldSize, setFieldSize] = useState('multiline');
+  const [fieldLabel, setFieldLabel] = useState('Here you can write notes:');
+  const [placeholder, setPlaceHolder] = useState('I am a sample text.');
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Create Textfield</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      {/* Top Buttons */}
+      <Stack distribution="fillEvenly">
+        <Stack.Item>
+          <Link href="#">
+            <a
+              style={{
+                color: '#000',
+                display: 'flex',
+                alignItems: 'center',
+                textDecoration: 'none',
+              }}
+            >
+              <span style={{ margin: 0 }}>
+                {' '}
+                <Icon source={HomeMajor} />
+              </span>
+              <Heading>
+                <span
+                  style={{
+                    marginLeft: '0.3em',
+                  }}
+                >
+                  Dashboard
+                </span>
+              </Heading>
+            </a>
+          </Link>
+        </Stack.Item>
+        <Stack.Item>
+          <div style={{ float: 'right' }}>
+            <Button size="slim" destructive>
+              Delete
+            </Button>
+          </div>
+        </Stack.Item>
+      </Stack>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      {/* Follow Guide Card */}
+      <div style={{ marginTop: '2em' }}>
+        <Card title="Set up your text field" sectioned>
+          <p style={{ color: 'gray' }}>
+            Follow this guide and you will be up and running in no time
+          </p>
+        </Card>
+      </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+      {/* Input starts */}
+      <div style={{ marginTop: '2em' }}>
+        <Layout>
+          <Layout.AnnotatedSection
+            title="Field Name"
+            description="Define a unique name."
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            <Card sectioned>
+              <FormLayout>
+                <TextField
+                  label="Field name"
+                  value={fieldName}
+                  onChange={(newValue) => setFieldName(newValue)}
+                />
+                <p>
+                  The name of this field will also be displayed in the shopping
+                  cart, checkout and order listing.{' '}
+                  <span style={{ fontWeight: 'bold' }}>
+                    {' '}
+                    To avoid overlaps, please name each upload field uniquely.
+                  </span>
+                </p>
+              </FormLayout>
+            </Card>
+          </Layout.AnnotatedSection>
+        </Layout>
+      </div>
+      <div style={{ marginTop: '2em' }}>
+        <Layout>
+          <Layout.AnnotatedSection
+            title="Live preview"
+            description="Here you can see how your text field will
+            look like."
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+            <Card sectioned>
+              <FormLayout>
+                {fieldSize === 'multiline' ? (
+                  <TextField
+                    label={fieldLabel}
+                    multiline={4}
+                    placeholder={placeholder}
+                  />
+                ) : (
+                  <TextField label={fieldLabel} placeholder={placeholder} />
+                )}
+              </FormLayout>
+            </Card>
+          </Layout.AnnotatedSection>
+        </Layout>
+      </div>
     </div>
-  )
+  );
 }
